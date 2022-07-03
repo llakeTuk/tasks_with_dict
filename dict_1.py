947a36896fdd46741a1561c1e03cbafab7e50974
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-07-03 09:15:18 +0300

    Add files via upload

---
+++ dict_1.py
@@ -0,0 +1,8 @@
+def to_dict(input_list):
+  return {item: item for item in input_list}
+y = 'y'
+while y == 'y':
+  input_list = input('enter list of elements, using comma: ').split(',')
+  print(to_dict(input_list))
+  y = input('restart?(y/n): ')
+print('goodbye')
