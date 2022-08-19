e4d9d2d0ddc18f756d98ae84162355cbfc54ae5c
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-08-19 11:48:20 +0300

    Update dict_3.py

--- dict_3.py
+++ dict_3.py
@@ -2,6 +2,7 @@ def count_it(str_numbers):
   num_count = {int(elem): str_numbers.count(elem) for elem in str_numbers}
   sorted_num_count = sorted(num_count.items(), key = lambda element: element[1])
   return dict(sorted_num_count[-3:])
+
 y = 'y'
 while y == 'y':
   input_str = input('enter string of numbers: ')
